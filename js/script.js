069fc30f9cfef124933063e83d392ebfea2e8c14
Ira <nehachu-ha@list.ru>
2020-12-01 13:37:23 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,115 @@
+"use strict";
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+
+// console.log(number);
+// const obj = {
+//     a: 50
+// };
+
+// obj.a = 10;
+// console.log(obj);
+
+
+// console.log(name);
+// var name = 'Ivan';
+// name = 'alex';
+
+// {
+// let result = 40;
+// }
+// console.log(result);
+
+// console.log(-4 / 0);
+// console.log('string' * 9);
+
+// const persone = 'string';
+
+// const bool = true;
+
+// console.log(smth); //  null
+
+// let und;
+// console.log(und); // undefined
+
+
+// const obj = {
+//  name: 'John',
+//  age: 25,
+//  isMarried: false
+// };
+
+// console.log(obj.name);
+// console.log(obj["name"]);
+
+// let array = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+// console.log(array[0]);
+
+// alert('hello');
+
+// const result = confirm('are you here?');
+// console.log(result);
+
+// const answer = prompt('are you 18?', '');
+// // console.log(answer);
+// console.log(typeof(answer));
+
+
+// const answer = +prompt('are you 18?', '');
+// console.log(typeof(answer));
+// console.log(answer + 5);
+
+// const answers = []; // запись в массив
+// answers[0] = prompt('what is your name?', '');
+// answers[1] = prompt('what is your surname?', '');
+// answers[2] = prompt('what is your age?', '');
+
+// document.write(answers);
+// console.log(typeof(answers));
+
+// console.log(typeof(null)); //   официально признанная ошибка в js
+
+
+// const category = 'toys';
+// console.log(`i need by ${category}`); 
+
+// const user = 'Ivan';
+// alert(`hello, ${user}`);
+
+// console.log('arr ' + '- object'); //конкатенация
+// console.log(4  + '- object');
+
+// console.log(4  + +'5');
+
+// let incr = 10,
+//     decr = 10;
+
+// // incr++;
+// // decr--; git
+// // console.log(incr);
+// // console.log(decr);
+
+// // ++incr;
+// // --decr;
+
+// console.log(incr++);
+// console.log(decr--);
+
+// // console.log(++incr);
+// // console.log(--decr);
+
+// console.log(5%2);
+
+// console.log(2*4 == '8');
+// console.log(2*4 === 8);
+
+// const isChecked = true;
+// const isClose = true;
+// console.log(isChecked && isClose);
+// console.log(isChecked || isClose);
+// console.log(isChecked || !isClose);
+
+// console.log(2*4 != 8);

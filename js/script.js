95c67d84e055ffdd7bb75da95d3d509bd2e74670
Ira <nehachu-ha@list.ru>
2020-12-07 12:35:56 +0300

    add string and number methods

--- js/script.js
+++ js/script.js
@@ -6,6 +6,9 @@
 // number = 10;
 
 // console.log(number);
+
+
+
 // const obj = {
 //     a: 50
 // };
@@ -148,7 +151,7 @@
 //                 break;
 //  };
 
-let num = 50;
+// let num = 50;
 // while (num <= 55) {
 //     console.log(num);
 //     num++;
@@ -160,15 +163,104 @@ let num = 50;
 // }
 // while (num < 55);
 
-for (let i = 1; i < 8; i++) {
-    console.log(num);
-     num++;
-}
+// for (let i = 1; i < 8; i++) {
+//     console.log(num);
+//      num++;
+// }
 
-for (let i = 1; i < 8; i++) {
-    if (i === 6) {
-        // break;
-        continue;
-    }
-    console.log(i);
-}
+// for (let i = 1; i < 8; i++) {
+//     if (i === 6) {
+//         // break;
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+
+
+
+
+// function showFirstMessage (text) {
+//     console.log(text);
+//     let num = 20;
+// }
+
+// showFirstMessage('hello!');
+// console.log(num);
+
+// let num = 20;
+
+// function showFirstMessage (text) {
+//     console.log(text);
+//     let num = 10;
+//     console.log(num); 
+// }
+
+// showFirstMessage('hello!');
+// console.log(num);
+
+// function calc(a, b) {
+//     return (a + b);
+//     console.log('gfdfghjk'); //анричебл кодб, код который никогда не выполнитсяб т.к. находится после  return, мертвый код;
+// }
+// console.log(calc(4, 3));
+// console.log(calc(7, 5));
+// console.log(calc(10, 6));
+
+
+// function ret() {
+//     let num = 50;
+//     return num;
+// }                   // возврат значения наружу ф-ции;
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+// const logger = function () {
+//     console.log('hello')
+// };
+// logger();
+
+
+// const calc = (a, b) => { return a + b };
+// //const calc = (a, b) => a + b;  
+// //const calc = a  => a + b;
+// //const calc = (a, b) => {
+// //      console.log('1');
+// //      return a + b;
+// //};
+
+
+
+// //свойство строк
+// const str = 'test';
+// console.log(str.length);
+// const arr = [1, 2, 3, 4, 5];
+// console.log(arr.length);
+
+// console.log(str[2]);  //получение символа из строки по индексу;
+
+// // методы строк
+// console.log(str.toUpperCase()); // приведение к верхнему регистру;
+// console.log(str.toLowerCase()); // приведение к нижнему регистру;
+
+// const fruit = 'some fruit';
+// console.log(fruit.indexOf('fruit')); // найти кусок строки и сказать с какой позиции она начинается; (поиск подстроки);  нумерация с 0; -1 - значит что искомая строка не найдена;
+
+// const logg = 'hello world!';
+// console.log(logg.slice(6, 11)); //вырезает кусок строки с : 1 аргумент  - индекс включается, 2 ой аргумент - индекс не включается; 
+// console.log(logg.slice(6));  //вырезает от указанного индекса до конца строки;
+// console.log(logg.slice(-5, -11)); //отсчет с конца строки; 
+
+// console.log(logg.substring(6, 11)); // похож на метод slice,  но нельзя использовать отрицат значения и не может быть 1ое значние больше второго;
+
+// console.log(logg.substr(6, 5)); // указывает с какого символа (1й аргумент) и сколько символов (2й аргумент) необходимо вырезать;
+
+
+
+// // методы чисел
+//  const num = 12.2;
+// console.log(Math.round(num)); //округление к ближайшему целому
+
+// const test = '12.2 px';
+// console.log(parseInt(test)); // переводит число в другую систему исчисления (здесь строку приводит к числу округленное)
+// console.log(parseFloat(test)); // берет строку и возвращает в 10тичном варианте (здесь строку приводит к числу неокругленное)

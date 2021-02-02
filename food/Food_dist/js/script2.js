70b4dd8e24b7338a3cf41d005557ec75483869d9
Ira <nehachu-ha@list.ru>
2021-02-02 13:18:19 +0300

    add async, await and reseivind data fron the server

--- food/Food_dist/js/script2.js
+++ food/Food_dist/js/script2.js
@@ -31,7 +31,7 @@ window.addEventListener('DOMContentLoaded', function() {
     const target = event.target;
     if(target && target.classList.contains('tabheader__item')) {
       tabs.forEach((item, i) => {
-        if (target == item) {
+        if (target === item) {
           hideTabContent();
           showTabContent(i);
         }
@@ -117,7 +117,7 @@ window.addEventListener('DOMContentLoaded', function() {
   }
 
   modal.addEventListener('click', (e) => {
-    if (e.target === modal || e.target.getAttribute('data-close') == "") {
+    if (e.target === modal || e.target.getAttribute('data-close') === "") {
       closeModal();
     }
   });
@@ -182,32 +182,79 @@ window.addEventListener('DOMContentLoaded', function() {
     }
   }
 
-  new MenuCard(
-    "img/tabs/vegy.jpg",
-    "vegy",
-    'Меню "Фитнес"',
-    'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
-    9,
-    ".menu .container"
-  ).render();
+  const getResource = async (url) => {   //создаем функцию которая будет получать данные с сервера
+    const res = await fetch(url);
+    if (!res.ok) {  // если выкидывать ошибку в ручном режиме, то сработает блок кода catch
+      //объект ошибки, внутрь помещается текст ошибки, чтобы выкинуть объект ошибки оспользуется оператор throw
+      throw new  Error(`Could not fetch ${url}, status: ${res.status}`); //выведется в консоль
+    }
+    return await res.json();
+  };
+  //если fetch столкнется с ошибкой в HTTP запросе (404 500 502) то он не выдаст catch(reject) (ошибкой для fetch является отсутствие интеренета или критические неполадки в самом запросе)
+  //у промиса, который возвращается из fetch есть два свойства:
+  //.ok - это свойство говорит о том что мы что-то получили и все ок или не ок
+  //status - здесь мы попадаем на тот статус который вернул нам сервер(200 - ok, 404 - not found, 500)
 
-  new MenuCard(
-    "img/tabs/post.jpg",
-    "post",
-    'Меню "Постное"',
-    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
-    14,
-    ".menu .container"
-  ).render();
+  getResource(' http://localhost:3000/menu')
+    .then(data => {
+      // data.forEach(obj => {
+      //   new MenuCard(obj.img, obj.altimg, obj.title, obj.descr, obj.price).render(); //этот конструктор будет создаваться столько раз, сколько объектов будет внутри массива, который придет с сервера
+      // });
+      // здесь должны использовать синтаксис деструктуризации объекта - те когда из объекта вытаскиваем отдельные свойствва в качестве отдельной переменной
+      data.forEach(({img, altimg, title, descr, price}) => {
+        new MenuCard(img, altimg, title, descr, price, '.menu .container').render(); //этот конструктор будет создаваться столько раз, сколько объектов будет внутри массива, который придет с сервера
+      });
+    });
+  // теперь код ниже абсолютно не нужен
+  // new MenuCard(
+  //   "img/tabs/vegy.jpg",
+  //   "vegy",
+  //   'Меню "Фитнес"',
+  //   'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+  //   9,
+  //   ".menu .container"
+  // ).render();
+  //
+  // new MenuCard(
+  //   "img/tabs/post.jpg",
+  //   "post",
+  //   'Меню "Постное"',
+  //   'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+  //   14,
+  //   ".menu .container"
+  // ).render();
+  //
+  // new MenuCard(
+  //   "img/tabs/elite.jpg",
+  //   "elite",
+  //   'Меню “Премиум”',
+  //   'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+  //   21,
+  //   ".menu .container"
+  // ).render();
 
-  new MenuCard(
-    "img/tabs/elite.jpg",
-    "elite",
-    'Меню “Премиум”',
-    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
-    21,
-    ".menu .container"
-  ).render();
+  // второй способ не будет использовать классы а будет формировать верстку на лету, этот метод можно использовать когда нужно построить что-то только 1 раз
+  // getResource(' http://localhost:3000/menu')
+  //   .then(data => createCard());
+  //
+  // function createCard (data) {
+  //   data.forEach(({img, altimg, title, descr, price}) => {
+  //     const element = document.createElement('div');
+  //     element.classList.add('.menu_item');
+  //     element.innerHTML = `
+  //               <img src=${img} alt=${altimg}>
+  //               <h3 class="menu__item-subtitle">${title}</h3>
+  //               <div class="menu__item-descr">${descr}</div>
+  //               <div class="menu__item-divider"></div>
+  //               <div class="menu__item-price">
+  //                   <div class="menu__item-cost">Цена:</div>
+  //                   <div class="menu__item-total"><span>${price}</span> грн/день</div>
+  //               </div>
+  //     `
+  //     document.querySelector('.menu .container').append(element);
+  //
+  //   });
+  // }
 
   // Forms
 
@@ -219,10 +266,25 @@ window.addEventListener('DOMContentLoaded', function() {
   };
 
   forms.forEach(item => {
-    postData(item);
+    bindPostData(item);
   });
 
-  function postData(form) {
+  //для преобразования асинхронного кода в синхронный оспользуются операторы c
+  //async указывается перед началом функции, то мы говорим что внутри функции будет какой-то асинхронный код,
+  // затем используем его парный оператор await его ставим перед теми операциями которые нам небходимо дождаться. время ожидания выполнения кода по стандарту 30 сек
+  //эти операторы всегда используются в паре
+  const postData = async (url, data) => {   //это асинхронный код, тк fetch отправляет запрос на сервер и сразу продолжается работа функции не дожидаясь ответа от сервера
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: data
+    });
+    return await res.json();
+  };
+
+  function bindPostData(form) {
     form.addEventListener('submit', (e) => {
       e.preventDefault();
 
@@ -236,20 +298,32 @@ window.addEventListener('DOMContentLoaded', function() {
 
       const formData = new FormData(form);
 
-      const object = {}; // не используется при передаче formData
-      formData.forEach(function(value, key){
-        object[key] = value;
-      });
+      // const object = {}; // не используется при передаче formData
+      // formData.forEach(function(value, key){
+      //   object[key] = value;
+      // });
 
-      fetch('server.php', {
-        method: 'POST',
-        headers: {   // не указываются при отправке formData
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(object)
-        //body: formData
-      })
-        .then(data => data.text()) //превращаем ответ в обычный текст
+      //существуют методы которые помогают преобразить formData в JSON
+      //метод entries - возвращает массив собственных перечисляемых свойств указанного объекта
+      // const obj = {a: 25, b: 15};
+      // console.log(Object.entries(obj)); // [['a', 25], ['b', 15]
+      //то этот метод возвращает массив в котором находятся другие массивы, те берет каждое свойство и формирует из него маленький массив
+      //метод fromEntries - превращает матрицу созданную с помощью entries в объект
+
+      const json = JSON.stringify(Object.fromEntries(formData.entries()));
+
+      //есть смысл всегда выносить функционал по общению с сервером выносить в отдельные функции (см postData)
+      // fetch('server.php', {
+      //   method: 'POST',
+      //   headers: {   // не указываются при отправке formData
+      //     'Content-Type': 'application/json'
+      //   },
+      //   body: JSON.stringify(object)
+      //   //body: formData
+      // })
+
+      postData(' http://localhost:3000/requests', json)
+        // .then(data => data.text()) //превращаем ответ в обычный текст
         .then(data => {
         console.log(data);
         showThanksModal(message.success);
